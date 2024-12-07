92fb9d6376432d67c04de7dd4c53b38841705c6d
hoangkhiem3k <ledohoangkhiem3k@gmail.com>
2024-12-07 09:29:13 +0700

    add-axios-function-get-tasks

---
+++ src/composables/logRequestError.ts
@@ -0,0 +1,12 @@
+import { AxiosError } from "axios";
+
+export default function logRequestError(
+  methodName: string,
+  err: AxiosError | unknown
+): void {
+  console.error(
+    `Error in method ${methodName}: ${
+      err instanceof Error ? err.message : String(err)
+    }`
+  );
+}

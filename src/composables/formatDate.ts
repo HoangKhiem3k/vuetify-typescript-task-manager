360fbe168003fd8836fb49b5fab5391921fe685c
hoangkhiem3k <ledohoangkhiem3k@gmail.com>
2024-12-07 15:50:06 +0700

    add format date hook navigate

---
+++ src/composables/formatDate.ts
@@ -0,0 +1,17 @@
+export const formatDate = (
+  date: string,
+  locale: string = "en-US",
+  options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  }
+): string => {
+  const parsedDate = new Date(date);
+  if (isNaN(parsedDate.getTime())) {
+    // Handle invalid date
+    console.warn("formattedDate: Invalid date provided");
+    return "";
+  }
+  return parsedDate.toLocaleDateString(locale, options).replace(/\//g, ".");
+};
